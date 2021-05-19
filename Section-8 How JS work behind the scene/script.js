918790ff283d381e1d50a7da0aadf9cb377c7b71
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-05-19 18:02:17 +0600

    updated

--- Section-8 How JS work behind the scene/script.js
+++ Section-8 How JS work behind the scene/script.js
@@ -1,8 +1,27 @@
 'use strict';
+//const and let are blocked scoped
+//function are also bloced scoped
+//var is function scoped
+
 function calcAge(birthYear){
   const age =2037-birthYear;
-  console.log(firstName);
+  function printAge(){
+    const output = `${firstName} You are ${age} years old, born in ${birthYear}`;
+    console.log(output);
+    if(birthYear=>1981 && birthYear<=1996){
+      var test = `i am function var type variable`
+      const str = `Oh, and you are a milleniar,${firstName}`;
+      function add(a,b){
+        return a+b;
+      }
+      console.log(str);
+      console.log(add(2,4));
+    }
+       console.log(test);
+      
+  }
+  printAge();
   return age;
 }
 const firstName = 'Jonas'
-console.log(calcAge(1991));
+console.log(calcAge(1992));
